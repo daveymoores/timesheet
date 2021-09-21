e104e6766c27a2a4b90f0bafdabd9feabbb2fd52
Davey Moores <daveymoores@gmail.com>
2021-09-21 13:06:48 +0200

    checking for and creating a configuration file

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,29 @@
 use git2::Repository;
 use regex::{Error, Regex};
+use std::fs::File;
+use std::io::{Read, Write};
 use std::{env, io, io::ErrorKind, process};
+
+use serde::{Deserialize, Serialize};
+use serde_json;
+
+const CONFIG_FILE_NAME: &str = ".timesheet-gen.txt";
+
+#[derive(Debug)]
+pub enum Commands {
+    Init,
+}
+
+impl std::str::FromStr for Commands {
+    type Err = String;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            "init" => Ok(Commands::Init),
+            _ => Err(format!("'{}' is not a valid value for Commands", s)),
+        }
+    }
+}
 
 #[derive(PartialEq, Debug)]
 pub struct Config {
@@ -41,7 +64,7 @@ impl Config {
 // Creates a new repository struct after being sent data from git2.
 // It returns the namespace and path, but also init date for the repo and probably other stuff
 // Basically sanitise the data from git2 into something usable
-#[derive(PartialEq, Debug)]
+#[derive(PartialEq, Debug, Serialize, Deserialize)]
 pub struct Repo {
     pub namespace: String,
     pub path: String,
@@ -83,9 +106,30 @@ pub fn use_existing_repository(option: Option<&str>) -> String {
     }
 }
 
+pub fn check_for_existing_config_file() {
+    let path = get_filepath();
+
+    match File::open(&path) {
+        Err(_) => println!("This looks like the first time you're running Timesheet."),
+        Ok(mut file) => {
+            let mut s = String::new();
+            match file.read_to_string(&mut s) {
+                Err(why) => panic!("couldn't read {}: {}", path, why),
+                Ok(_) => {
+                    let mut input = String::new();
+                    println!("We've found an existing configuration at:\n{}\n", path);
+                    println!("Would you like to use this configuration? (Y/n)");
+                    read_input(&mut input);
+                    process::exit(1);
+                }
+            }
+        }
+    };
+}
+
 // TODO - should consider using git2 "discover" so that a repository
 // can be suggested if the user isn't in the correct repository
-pub fn find_repository_details(path: &str) -> Repo {
+fn find_repository_details(path: &str) -> Repo {
     let repo = match Repository::open(path) {
         Ok(repo) => repo,
         Err(e) => panic!("failed to open: {}", e),
@@ -95,6 +139,31 @@ pub fn find_repository_details(path: &str) -> Repo {
         eprintln!("Repo not found: {}", err);
         process::exit(1);
     })
+}
+
+fn get_filepath() -> String {
+    let home = match dirs::home_dir() {
+        Some(arg) => arg,
+        None => panic!("Home directory not found"),
+    };
+    let home_string = home.to_str();
+    home_string.unwrap().to_owned() + "/" + CONFIG_FILE_NAME
+}
+
+pub fn create_user_config(path: &str) {
+    let repo: Repo = find_repository_details(&*path);
+    let json = serde_json::to_string(&repo).unwrap();
+    let path = get_filepath();
+
+    let mut file = match File::create(&path) {
+        Err(why) => panic!("couldn't create {}: {}", path, why),
+        Ok(file) => file,
+    };
+
+    match file.write_all(json.as_bytes()) {
+        Err(why) => panic!("couldn't write to {}: {}", path, why),
+        Ok(_) => println!("successfully wrote to {}", path),
+    }
 }
 
 #[cfg(test)]
--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,29 @@
 extern crate git2;
-
 use std::env;
 use std::process;
 
-use timesheet::{find_repository_details, read_input, use_existing_repository, Config, Repo};
+use timesheet::{
+    check_for_existing_config_file, create_user_config, read_input, use_existing_repository,
+    Commands, Config,
+};
 
 fn main() {
     let mut input: String = String::new();
 
+    // Construct Config struct with repo path and commands
     let config: Config = Config::new(env::args()).unwrap_or_else(|err| {
         eprintln!("Problem parsing arguments: {}", err);
         process::exit(1);
     });
 
+    // Match the command against an enum of cli commands
+    let command: Commands = config.command.parse().unwrap();
+    match command {
+        Commands::Init => check_for_existing_config_file(),
+    }
+
+    // If command isn't found, check go through on boarding process
+    // and generate timesheet config file
     let path = match config.repository_path {
         Some(arg) => arg,
         None => {
@@ -27,7 +38,5 @@ fn main() {
         process::exit(1);
     }
 
-    let repo: Repo = find_repository_details(&*path);
-
-    println!("namespace: {:?} path: {:?}", repo.namespace, repo.path);
+    create_user_config(&*path);
 }
